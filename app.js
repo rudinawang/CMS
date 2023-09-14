1958cabe53c6cd6b7533bb0bd2c94613928772b3
rudinawang <rudnawang07@gmail.com>
2023-09-14 14:55:22 +0545

    last commit

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // All Blogs
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  // table batw data nikalnu paryo
+  // blogs vannay table batw vaye jati ko data day vanayrw vanayko
+  const allBlogs = await blogs.findAll();
+  console.log(allBlogs);
   res.render("blogs.ejs");
 });
 
@@ -42,8 +46,10 @@ app.post("/createBlog", async (req, res) => {
     description: description,
   });
 
-  res.send("<h1>Form Submitted Successfully</h1>");
+  res.redirect("/");
 });
+
+// Database ko table batw data kasari nikalnay
 
 app.listen(3000, () => {
   console.log("Node.JS project has started at port 3000");
