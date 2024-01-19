cd0308426b9b9ceab368ef75f8d5406305e68f63
rudinawang <rudnawang07@gmail.com>
2024-01-19 21:11:51 +0545

    add sql

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// database Connection
+require("./model/index");
+
 // tellling nodejs to set the env for view engine
 app.set("view engine", "ejs");
 
